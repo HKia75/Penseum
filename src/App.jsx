25631bcd17da80f7c8da0ee9e271ac6554102988
HKia <hkia.org536@gmail.com>
2023-09-03 12:58:40 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <>
+      <div className="flex m-4 text-left  justify-center"></div>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
